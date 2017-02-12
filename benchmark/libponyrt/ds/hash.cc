e5397450b78b8682ab334ca83eb0271dba4519f0
dipinhora <dipin@sendence.com>
2017-02-12 00:49:53 +0000

    Optimize runtime quadratic hashmap algorithm
    Update objectmap sweep and actormap sweep to use clearindex and
    optimize.
    
    Don't destroy/recreate `ctx->acquire` all the time.
    
    Additionally, updated destroy and resize to use ponyint_hashmap_next
    to efficiently scan items for freeing and copying to new bucket array.

--- benchmark/libponyrt/ds/hash.cc
+++ benchmark/libponyrt/ds/hash.cc
@@ -26,6 +26,7 @@ class HashMapBench: public ::benchmark::Fixture
     hash_elem_t* get_element();
     void put_elements(size_t count);
     void delete_elements(size_t del_count, size_t count);
+    void clear_elements(size_t del_count, size_t count);
 
   public:
     static size_t hash_tst(hash_elem_t* p);
@@ -91,6 +92,22 @@ void HashMapBench::delete_elements(size_t del_pct, size_t count)
       free_elem(d);
   }
   free_elem(e1);
+}
+
+void HashMapBench::clear_elements(size_t del_pct, size_t count)
+{
+  size_t del_count = (del_pct * count) / 100;
+  size_t final_count = (del_count > count) ? 0 : count - del_count;
+
+  // clear random items until map size is as small as required
+  while(testmap_size(&_map) > final_count)
+  {
+    size_t index = rand() % _map.contents.size;
+
+    hash_elem_t* d = testmap_clearindex(&_map, index);
+    if(d != NULL)
+      free_elem(d);
+  }
 }
 
 hash_elem_t* HashMapBench::get_element()
@@ -397,6 +414,113 @@ BENCHMARK_DEFINE_F(HashMapBench, HashSearchDeletes)(benchmark::State& st) {
 }
 
 BENCHMARK_REGISTER_F(HashMapBench, HashSearchDeletes)
+  ->Args({1, 1<<10, 64, 0})
+  ->Args({1, 2<<10, 64, 0})
+  ->Args({1, 4<<10, 64, 0})
+  ->Args({1, 8<<10, 64, 0})
+  ->Args({1, 16<<10, 64, 0})
+  ->Args({1, 32<<10, 64, 0})
+  ->Args({1, 1<<10, 90, 0})
+  ->Args({1, 2<<10, 90, 0})
+  ->Args({1, 4<<10, 90, 0})
+  ->Args({1, 8<<10, 90, 0})
+  ->Args({1, 16<<10, 90, 0})
+  ->Args({1, 32<<10, 90, 0})
+  ->Args({1, 1<<10, 99, 0})
+  ->Args({1, 2<<10, 99, 0})
+  ->Args({1, 4<<10, 99, 0})
+  ->Args({1, 8<<10, 99, 0})
+  ->Args({1, 16<<10, 99, 0})
+  ->Args({1, 32<<10, 99, 0})
+  ->Args({1, 1<<10, 101, 0})
+  ->Args({1, 2<<10, 101, 0})
+  ->Args({1, 4<<10, 101, 0})
+  ->Args({1, 8<<10, 101, 0})
+  ->Args({1, 16<<10, 101, 0})
+  ->Args({1, 32<<10, 101, 0})
+  ->Args({1, 1<<10, 64, 1})
+  ->Args({1, 2<<10, 64, 1})
+  ->Args({1, 4<<10, 64, 1})
+  ->Args({1, 8<<10, 64, 1})
+  ->Args({1, 16<<10, 64, 1})
+  ->Args({1, 32<<10, 64, 1})
+  ->Args({1, 1<<10, 90, 1})
+  ->Args({1, 2<<10, 90, 1})
+  ->Args({1, 4<<10, 90, 1})
+  ->Args({1, 8<<10, 90, 1})
+  ->Args({1, 16<<10, 90, 1})
+  ->Args({1, 32<<10, 90, 1})
+  ->Args({1, 1<<10, 99, 1})
+  ->Args({1, 2<<10, 99, 1})
+  ->Args({1, 4<<10, 99, 1})
+  ->Args({1, 8<<10, 99, 1})
+  ->Args({1, 16<<10, 99, 1})
+  ->Args({1, 32<<10, 99, 1})
+  ->Args({1, 1<<10, 101, 1})
+  ->Args({1, 2<<10, 101, 1})
+  ->Args({1, 4<<10, 101, 1})
+  ->Args({1, 8<<10, 101, 1})
+  ->Args({1, 16<<10, 101, 1})
+  ->Args({1, 32<<10, 101, 1});
+
+BENCHMARK_DEFINE_F(HashMapBench, HashSearchOptimized)(benchmark::State& st) {
+  // range(2) == % of items to delete at random
+  clear_elements(st.range(2), st.range(1));
+  hash_elem_t* e1 = get_element();
+  size_t map_count = testmap_size(&_map);
+  // we're testing our pathological case
+  if(map_count == 0)
+  {
+    for(size_t i = 0; i < _map.contents.size; i++) {
+      e1->key = i;
+
+      // putindex item
+      testmap_putindex(&_map, e1, i);
+
+      // remove item
+      testmap_clearindex(&_map, i);
+    }
+    // put item so it's not empty
+    hash_elem_t* e2 = get_element();
+    e2->key = rand();
+    testmap_put(&_map, e2);
+  }
+  testmap_optimize(&_map);
+  size_t *a = NULL;
+  a = new size_t[testmap_size(&_map)];
+  size_t ind = HASHMAP_UNKNOWN;
+  for(size_t i = 0; i < testmap_size(&_map); i++) {
+    hash_elem_t* n = testmap_next(&_map, &ind);
+    if(n != NULL) {
+      a[i] = n->key;
+    } else {
+      st.SkipWithError("Item shouldn't be NULL!");
+    }
+  }
+  while (st.KeepRunning()) {
+    st.PauseTiming();
+    // exclude random # time
+    if(st.range(3) == 0)
+      e1->key = a[rand() % testmap_size(&_map)];
+    else
+      e1->key = rand();
+    size_t index = HASHMAP_UNKNOWN;
+    st.ResumeTiming();
+    hash_elem_t* n2 = testmap_get(&_map, e1, &index);
+    st.PauseTiming();
+    if(st.range(3) == 0 && n2 == NULL)
+    {
+      st.SkipWithError("Item shouldn't be NULL!");
+    }
+    st.ResumeTiming();
+  }
+  st.SetItemsProcessed(st.iterations());
+  delete[] a;
+  free_elem(e1);
+  e1 = nullptr;
+}
+
+BENCHMARK_REGISTER_F(HashMapBench, HashSearchOptimized)
   ->Args({1, 1<<10, 64, 0})
   ->Args({1, 2<<10, 64, 0})
   ->Args({1, 4<<10, 64, 0})

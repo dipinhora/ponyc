362dd955a821bddbb84d0ccfe628009e71d5248c
dipinhora <dipin@sendence.com>
2017-12-22 09:02:48 +0000

    enable verbose builds

--- .travis_script.bash
+++ .travis_script.bash
@@ -11,12 +11,12 @@ fi
 
 ponyc-test(){
   echo "Building and testing ponyc..."
-  make CC="$CC1" CXX="$CXX1" test-ci
+  make CC="$CC1" CXX="$CXX1" verbose=1 test-ci
   if [[ "${CROSS_ARCH}" != "" ]]
   then
     echo "Building and testing cross ponyc..."
-    make  CC="${CROSS_CC}" CXX="${CROSS_CXX}" arch="${CROSS_ARCH}" bits="${CROSS_BITS}" CFLAGS="${CROSS_CFLAGS}" CXXFLAGS="${CROSS_CXXFLAGS}" LDFLAGS="${CROSS_LDFLAGS}" libponyrt
-    make test-cross-ci PONYPATH=/usr/cross/lib cross_triple="${CROSS_TRIPLE}" cross_arch="${CROSS_ARCH}" cross_linker="${CROSS_LINKER}"
+    make verbose=1 CC="${CROSS_CC}" CXX="${CROSS_CXX}" arch="${CROSS_ARCH}" bits="${CROSS_BITS}" CFLAGS="${CROSS_CFLAGS}" CXXFLAGS="${CROSS_CXXFLAGS}" LDFLAGS="${CROSS_LDFLAGS}" libponyrt
+    make verbose=1 test-cross-ci PONYPATH=/usr/cross/lib cross_triple="${CROSS_TRIPLE}" cross_arch="${CROSS_ARCH}" cross_linker="${CROSS_LINKER}"
   fi
 }
 

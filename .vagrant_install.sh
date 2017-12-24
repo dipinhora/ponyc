fcc20b3814681614858da7f2fbe586bc26188658
dipinhora <dipin@sendence.com>
2017-12-24 20:38:28 +0000

    vagrant

---
+++ .vagrant_install.sh
@@ -0,0 +1,110 @@
+#! /bin/bash
+
+set -o errexit
+set -o nounset
+
+# from https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/templates/header.sh#L243-L260
+ANSI_RED="\\033[31;1m"
+ANSI_RESET="\\033[0m"
+
+travis_retry() {
+  local result=0
+  local count=1
+  while [ $count -le 3 ]; do
+    [ $result -ne 0 ] && {
+      echo -e "\\n${ANSI_RED}The command \"$*\" failed. Retrying, $count of 3.${ANSI_RESET}\\n" >&2
+    }
+    "$@" && { result=0 && break; } || result=$?
+    count=$((count + 1))
+    sleep 1
+  done
+  [ $count -gt 3 ] && {
+    echo -e "\\n${ANSI_RED}The command \"$*\" failed 3 times.${ANSI_RESET}\\n" >&2
+  }
+  return $result
+}
+
+download_vagrant(){
+  echo "Downloading and installing vagrant/virtualbox..."
+  travis_retry wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+  echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -s -c) contrib" | sudo tee -a /etc/apt/sources.list
+  travis_retry sudo apt-get -qq update
+  travis_retry sudo apt-get install virtualbox-5.2
+  travis_retry wget "https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.deb"
+  sudo dpkg -i vagrant_2.0.1_x86_64.deb
+  travis_retry vagrant init ubuntu/trusty32
+  travis_retry vagrant up
+}
+
+download_compiler(){
+  echo "Downloading and installing the compiler..."
+
+  travis_retry sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+  travis_retry sudo apt-get -qq update
+  travis_retry sudo apt-get install -y "${ICC1}" "${ICXX1}"
+}
+
+download_llvm(){
+  echo "Downloading and installing the LLVM specified by envvars..."
+
+  echo "deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty main" | sudo tee -a /etc/apt/sources.list
+  echo "deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty main" | sudo tee -a /etc/apt/sources.list
+  travis retry wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+  travis_retry sudo apt-get -qq update
+  travis_retry sudo apt-get install -y llvm-3.9
+}
+
+download_libunwind(){
+  echo "Downloading and building libunwind..."
+
+  travis_retry wget "http://download.savannah.nongnu.org/releases/libunwind/libunwind-1.2.1.tar.gz"
+  tar -xzf libunwind-1.2.1.tar.gz
+  pushd libunwind-1.2.1 && ./configure --prefix=/usr && make && sudo make install
+  popd
+  pushd libunwind-1.2.1 && ./configure --prefix=/usr/local && make && sudo make install
+  popd
+}
+
+download_pcre(){
+  echo "Downloading and building PCRE2..."
+
+  travis_retry wget "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-10.21.tar.bz2"
+  tar -xjf pcre2-10.21.tar.bz2
+  pushd pcre2-10.21 && ./configure --prefix=/usr && make && sudo make install
+  popd
+}
+
+set_linux_compiler(){
+  echo "Setting $ICC1 and $ICXX1 as default compiler"
+
+  sudo update-alternatives --install /usr/bin/gcc gcc "/usr/bin/$ICC1" 60 --slave /usr/bin/g++ g++ "/usr/bin/$ICXX1"
+}
+
+echo "Installing ponyc build dependencies..."
+
+case "${VAGRANT_ENV}" in
+
+  "ubuntu-i686")
+    if [[ "$(uname -i)" == "x86_64" ]]
+    then
+      download_vagrant
+      vagrant ssh -c "cp -r /vagrant ~/"
+      vagrant ssh -c "bash env ICC1=${ICC1} ICXX1=${ICXX1} .vagrant_install.sh"
+      vagrant ssh -c "bash make CC=\"$CC1\" CXX=\"$CXX1\" config=debug verbose=1 test-ci"
+      vagrant ssh -c "bash make CC=\"$CC1\" CXX=\"$CXX1\" config=release verbose=1 test-ci"
+    else
+      download_compiler
+      download_llvm
+      download_pcre
+      download_libressl
+      download_libunwind
+      set_linux_compiler
+    fi
+  ;;
+
+  *)
+    echo "ERROR: An unrecognized vagrant environment was found! VAGRANT_ENV: ${VAGRANT_ENV}"
+    exit 1
+  ;;
+
+esac

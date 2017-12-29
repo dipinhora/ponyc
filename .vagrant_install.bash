a29f71fce9e36f484586538ac99824679c0def48
dipinhora <dipin@sendence.com>
2017-12-29 04:33:12 +0000

    m

--- .vagrant_install.bash
+++ .vagrant_install.bash
@@ -118,8 +118,8 @@ case "${VAGRANT_ENV}" in
     sudo vagrant ssh -c "cat /proc/cpuinfo"
     sudo vagrant ssh -c "dmesg"
     sudo vagrant ssh -c "cd /vagrant && env VAGRANT_ENV=${VAGRANT_ENV}-install ICC1=${ICC1} ICXX1=${ICXX1} CC1=${CC1} CXX1=${CXX1} bash .vagrant_install.bash"
-    sudo vagrant ssh -c "cd /vagrant && env make CC=\"$CC1\" CXX=\"$CXX1\" config=debug verbose=1 test-ci"
-    sudo vagrant ssh -c "cd /vagrant && env make CC=\"$CC1\" CXX=\"$CXX1\" config=release verbose=1 test-ci"
+    sudo vagrant ssh -c "cd /vagrant && make CC=\"$CC1\" CXX=\"$CXX1\" config=debug verbose=1 test-ci"
+    sudo vagrant ssh -c "cd /vagrant && make CC=\"$CC1\" CXX=\"$CXX1\" config=release verbose=1 test-ci"
   ;;
 
   "ubuntu-i686-install")
@@ -136,6 +136,8 @@ case "${VAGRANT_ENV}" in
     cat /proc/cpuinfo
     sudo vagrant ssh -c "cd /vagrant && ls -laF"
     sudo vagrant ssh -c "cd /vagrant && env VAGRANT_ENV=${VAGRANT_ENV}-install bash .vagrant_install.bash"
+    sudo vagrant ssh -c "cd /vagrant && gmake config=debug verbose=1 test-ci"
+    sudo vagrant ssh -c "cd /vagrant && gmake config=release verbose=1 test-ci"
   ;;
 
   "freebsd-x86_64-install")
@@ -144,8 +146,6 @@ case "${VAGRANT_ENV}" in
     travis_retry sudo pkg install -y llvm39
     travis_retry sudo pkg install -y pcre2
     travis_retry sudo pkg install -y libunwind
-    sudo vagrant ssh -c "cd /vagrant && gmake config=debug verbose=1 test-ci"
-    sudo vagrant ssh -c "cd /vagrant && gmake config=release verbose=1 test-ci"
   ;;
 
   *)

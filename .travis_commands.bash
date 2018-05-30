6948276953e96cb96e3727611da4075d1df56b0d
Dipin Hora <dipin@sendence.com>
2018-05-30 19:15:01 +0000

    m

--- .travis_commands.bash
+++ .travis_commands.bash
@@ -8,14 +8,49 @@ ponyc-test(){
   make CC="$CC1" CXX="$CXX1" test-ci
 }
 
-ponyc-build-packages(){
-  echo "Installing ruby, rpm, and fpm..."
-  rvm use 2.2.3 --default
-  sudo apt-get install -y rpm
-  gem install fpm
+build_and_submit_deb_src(){
+  deb_distro=$1
+  rm -f debian/changelog
+  dch --package ponyc -v ${package_version}-1ubuntu1~${deb_distro}1 -D ${deb_distro} --controlmaint --create "Release ${package_version}"
+  debuild -S
+  dput ppa:dipinhora/testppa ../ponyc_${package_version}-1ubuntu1~${deb_distro}1_source.changes
+}
 
-  echo "Building ponyc packages for deployment..."
-  make CC="$CC1" CXX="$CXX1" verbose=1 arch=x86-64 tune=intel package_name="ponyc" package_base_version="$(cat VERSION)" deploy
+ponyc-build-packages(){
+  package_version=$(cat VERSION)
+
+#  echo "Installing ruby, rpm, and fpm..."
+#  rvm use 2.2.3 --default
+#  sudo apt-get install -y rpm
+#  gem install fpm
+
+#  echo "Building ponyc packages for deployment..."
+#  make CC="$CC1" CXX="$CXX1" verbose=1 arch=x86-64 tune=intel package_name="ponyc" package_base_version="${package_version}" deploy
+
+  echo "Install debuild, dch, dput..."
+  sudo apt-get install -y devscripts build-essential lintian
+
+  echo "Decrypting and Importing gpg keys..."
+  openssl aes-256-cbc -K $encrypted_0f44361077f1_key -iv $encrypted_0f44361077f1_iv -in gpg-files.tar.enc -out gpg-files.tar -d
+  tar -xvf gpg-files.tar
+  gpg --import dipin-secret-gpg.key
+  gpg --import-ownertrust dipin-ownertrust-gpg.txt
+
+  echo "Kicking off ponyc packaging for PPA and COPR..."
+  wget https://github.com/ponylang/ponyc/archive/${package_version}.tar.gz -O ponyc_${package_version}.orig.tar.gz
+  tar -xvzf ponyc_${package_version}.orig.tar.gz
+  cd ponyc_${package_version}
+  cp -r .packaging/deb debian
+  cp LICENSE debian/copyright
+
+  build_and_submit_deb_src xenial
+  build_and_submit_deb_src trusty
+  build_and_submit_deb_src artful
+  build_and_submit_deb_src bionic
+  build_and_submit_deb_src cosmic
+
+  # COPR for fedora/centos/suse
+  docker run -it --rm -e COPR_LOGIN=${COPR_LOGIN} -e COPR_USERNAME=${COPR_USERNAME} -e COPR_TOKEN=${COPR_TOKEN} -e COPR_COPR_URL=${COPR_COPR_URL} mgruener/copr-cli buildscm --clone-url https://github.com/dipinhora/ponyc --commit ${package_version} --subdir /.packaging/rpm/ --spec ponyc.spec --type git --nowait testcopr
 }
 
 ponyc-build-docs(){
